b5c88ddc39c95b5593102288d4f0d135184fa189
studyonlyonline <studyonlyonline@gmail.com>
2016-02-27 08:50:27 +0530

    Update take_a_break.py

--- take_a_break.py
+++ take_a_break.py
@@ -5,6 +5,6 @@ import time
 i=0
 print "this program started at "+time.ctime()
 while(i<3):    
-    time.sleep(3);
+    time.sleep(2*60*60);
     webbrowser.open("https://www.youtube.com/watch?v=xlUFkMKSB3Y&index=1&list=PL0862D1A947252D20");
     i+=1;
