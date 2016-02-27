feb70ba620c94e9d630253240b6eaf83fcccc561
studyonlyonline <studyonlyonline@gmail.com>
2016-02-27 08:49:24 +0530

    Take a break project
    It opens up web browser and plays the favourite video after every 2 hrs of my working on the laptop
    #inspired from programming in python tutorials on udaity

---
+++ take_a_break.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+#opening the web browser
+i=0
+print "this program started at "+time.ctime()
+while(i<3):    
+    time.sleep(3);
+    webbrowser.open("https://www.youtube.com/watch?v=xlUFkMKSB3Y&index=1&list=PL0862D1A947252D20");
+    i+=1;
