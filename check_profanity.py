4004fe47bc98378a8e1050e5d055e35fe84db7cc
studyonlyonline <studyonlyonline@gmail.com>
2016-03-03 20:08:42 +0530

    Profanity checker
    It checks the curse words present in the document.
    If true then it returns Profanity alert else it says that it is fine

---
+++ check_profanity.py
@@ -0,0 +1,20 @@
+import urllib
+def read_text():
+    quotes=open("F:\competetive coding\python_programming\movie_quotes.txt")
+    contents_of_file=quotes.read()
+    print contents_of_file
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection=urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output=connection.read()
+    print output
+    connection.close()
+    if "true" in output:
+        print "profanity alert"
+    else:
+        print "its fine"
+
+read_text()
+ 
