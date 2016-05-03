4eb108d3d66f5106fc64e45ca190b41cb0742ff7
Sarthak Gupta <studyonlyonline@gmail.com>
2016-05-03 21:42:24 +0530

    Update index.py

--- flask_shopsmart/index.py
+++ flask_shopsmart/index.py
@@ -16,8 +16,8 @@ app.secret_key='my_key'
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'studyonlyonline@gmail.com'
-app.config["MAIL_PASSWORD"] = 'samtron56v'
+app.config["MAIL_USERNAME"] = 'your_email_id_here'
+app.config["MAIL_PASSWORD"] = 'your_email_id_password'
 mail.init_app(app)
 
 def show_category():
